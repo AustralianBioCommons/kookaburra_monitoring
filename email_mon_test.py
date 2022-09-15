0fae7938d270732e0331dc27e5d77c1a0ce95278
astott <audrey.stott@pawsey.org.au>
2022-09-15 17:09:10 +0800

    test for receipt of email from Tower

---
+++ email_mon_test.py
@@ -0,0 +1,35 @@
+import imaplib
+import email
+import getpass
+from datetime import date
+from email.header import decode_header
+from linecache import getline
+
+# Assign variables for IMAP values
+imap_host = 'mail-server.pawsey.org.au'
+imap_user = 'kookaburra-mon'
+imap_pass =  getpass.getpass()
+imap = imaplib.IMAP4_SSL(imap_host)
+
+# Assign variable for today's date
+today_as_str = date.today().strftime("%d %b %Y")
+
+# Log in to server
+imap.login(imap_user, imap_pass)
+
+# Select email inbox
+imap.select('Inbox')
+
+# Put into a variable the list of messages with the header subject "Nextflow Tower Sign in"
+tmp, messages = imap.search(None, 'ALL', 'HEADER Subject "Nextflow Tower Sign in"')
+
+# Fetch the messages by its ID
+# Then print the date and time of all messages with the above header 
+for num in messages[0].split():
+    tmp, data = imap.fetch(num, '(RFC822)')
+    for response in data:
+        if isinstance(response, tuple):
+            msg = email.message_from_bytes(response[1])
+            dates = decode_header(msg["Date"])[0][0]
+            if today_as_str in dates:
+                print("True")
