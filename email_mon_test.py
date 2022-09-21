9981ff48d682a8716c66b080935b53681bd9eeac
Uwe Winter <uwwint@gmail.com>
2022-09-21 15:51:16 +1000

    now with external password

--- email_mon_test.py
+++ email_mon_test.py
@@ -7,7 +7,7 @@ from email.header import decode_header
 # Assign variables for IMAP values
 imap_host = 'mail-server.pawsey.org.au'
 imap_user = 'kookaburramon'
-imap_pass =  getpass.getpass()
+imap_pass =  open('external/password').readline().strip()
 imap = imaplib.IMAP4_SSL(imap_host)
 
 # Assign variable for today's date
