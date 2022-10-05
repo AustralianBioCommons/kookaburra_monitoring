8ff7d0145493f7c27f6b3c0ed845689e7cfc1a49
Benjamin Arntzen <barntzen@pawsey.org.au>
2022-10-05 13:54:14 +0800

    Check in today's session

--- testing/__init__.py
+++ testing/__init__.py
@@ -3,13 +3,16 @@ from selenium.common.exceptions import *
 from selenium.webdriver import *
 import imaplib
 import email
-from datetime import date
+from datetime import date,datetime,timedelta
+from urllib.parse import urlparse
 from email.header import decode_header
 from typing import Optional
 import traceback
 from time import sleep
+import re
 import sys
-from datetime import datetime
+from email import message_from_bytes
+from email.policy import default
 
 def check_email() -> Optional[str]:
     """
@@ -26,7 +29,8 @@ def check_email() -> Optional[str]:
     imap = imaplib.IMAP4_SSL(imap_host)
 
     # Assign variable for today's date
-    today_as_str = date.today().strftime("%d %b %Y")
+    today_as_str = date.today().strftime("%-d %b %Y")
+    print(today_as_str)
 
     # Log in to server
     imap.login(imap_user, imap_pass)
@@ -48,16 +52,44 @@ def check_email() -> Optional[str]:
             if isinstance(response, tuple):
                 msg = email.message_from_bytes(response[1])
                 dates = decode_header(msg["Date"])[0][0]
-                current_time = datetime.now()
-                # TODO - check if we need %d or %-d - see https://www.programiz.com/python-programming/datetime/strptime
-                a = datetime.strptime(dates, '%a, %d %b %Y %H:%M:%S %z (%Z)')
-                # TODO - compare a to current_time
+                email_received_time = datetime.strptime(dates, '%a, %d %b %Y %H:%M:%S %z (%Z)')
+                current_time = datetime.now(email_received_time.tzinfo)
+                # Calculate the earliest the email could have been received in order to be "within last 10 minutes"
+                min_received_time = current_time - timedelta(minutes=10)
+                # If the email received time occurs after that calculated time, it was received in the chosen period.
+                if email_received_time > min_received_time:
+                    # Email was in our chosen time period, so add it to the list
+                    if msg.is_multipart():
+                        for part in msg.walk():
+                            ctype = part.get_content_type()
+                            cdispo = str(part.get('Content-Disposition'))
 
-                if today_as_str in dates:
-                    recent_received_emails.append(data)
+                            # skip any text/plain (txt) attachments
+                            if ctype == 'text/plain' and 'attachment' not in cdispo:
+                                body = part.get_payload(decode=True)  # decode
+                                break
+                    else:
+                        print("Something weird has happened with the email received")
+                        return None
+                    recent_received_emails.append(body)
+                    recent_received_emails.append(body)
 
-    print(recent_received_emails[-1])
+    # Check if list is empty
+    if recent_received_emails:
+        # Turn the most recently received email into a plain multiline string
+        email_to_parse = recent_received_emails[-1].decode('utf-8')
+        # Parse the most recent email for a URL and return the URLs found in it
+        urls = (re.findall(r'(https?://[^\s]+)', email_to_parse))
+        # For each URL that we found...
+        for url in urls:
+            # Check the domain name...
+            domain = urlparse(url).netloc
+            # If it's Tower (therefore not likely a malicious link...)
+            if domain == "tower.services.biocommons.org.au":
+                return url
 
+    else:
+        return None
 
 def test_login():
     """This function tests the login in the following steps:
@@ -79,7 +111,14 @@ def test_login():
         success_div = browser.find_element(By.CSS_SELECTOR, "div.alert-success")
 
         #Step 2
-
+        # Wait for the email to be received (TODO: Smarter wait?)
+        sleep(4)
+        # Check for the new email and save it as login_url
+        login_url = check_email()
+        print(login_url)
+        # If we have a valid login URL, navigate to it using Selenium
+        browser.get(login_url)
+        # Step 3
 
     except Exception as e:
         #Todo: alert!
@@ -89,5 +128,5 @@ def test_login():
 
         pass
 
-#test_login()
-check_email()
+# Trigger a login via a remote controlled Firefox
+test_login()
