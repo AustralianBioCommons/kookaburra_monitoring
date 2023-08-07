be8bcf5683720c0933b22be6500c8786b3dacfaf
Uwe Winter <uwwint@gmail.com>
2023-08-07 11:18:17 +1000

    add -heartbeat

--- testing/__init__.py
+++ testing/__init__.py
@@ -139,6 +139,9 @@ def test_login():
         # Login to Slack
         slack_token = open('/home/ubuntu/kookaburra_monitoring/external/slacktoken').readline().strip()
         client = WebClient(token=slack_token)
+        if '-heartbeat' in sys.argv:
+            send_slack_message('Alive')
+            return
         #Step 1
         print("Opening web browser to main Tower page")
         display = Display(visible=0, size=(800, 600))
